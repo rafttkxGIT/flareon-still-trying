31ea5926f44bd0cb731d24a14326ecdbff848ba0
rafttkxGIT <rafa.m.gab@gmail.com>
2026-09-15 21:31:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def delete_auto_reply(id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
