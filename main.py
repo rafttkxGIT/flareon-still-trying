bb46e3a1b109baae68296d155d70149438ec13f4
rafttkxGIT <rafa.m.gab@gmail.com>
2026-09-15 20:02:47 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import threading
+import app
+import bot
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+
+def run_bot():
+    bot.main()
+
+if __name__ == '__main__':
+    flask_thread = threading.Thread(target=run_flask, daemon=True)
+    flask_thread.start()
+
+    run_bot()
